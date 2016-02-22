193f8c30b2b9b7d910969b416d5782dd41ec5c1d
Deep Mehta <deepmehta.it@live.com>
2016-02-21 20:25:16 -0800

    Initial Android Commit.

---
+++ Android/Phase1/ConnectOutpatient/app/src/main/java/iot/connect/com/connectoutpatient/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package iot.connect.com.connectoutpatient.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import iot.connect.com.connectoutpatient.R;
+
+/**
+ * Created by Deep on 15-Jan-16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
