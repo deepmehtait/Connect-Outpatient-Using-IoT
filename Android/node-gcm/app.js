8a1ec1359829f5600c8c09bbd71d35c88a4b190e
Deep Mehta <deepmehta.it@live.com>
2016-04-05 22:23:06 -0700

    Updated Project ID and Reg Token of the Application

--- Android/node-gcm/app.js
+++ Android/node-gcm/app.js
@@ -5,8 +5,9 @@ message.addNotification({
   body: 'Patient is in serious condition.!',
   icon: 'ic_stat_emergency'
 });
-var regTokens = ['doFQIS89FJg:APA91bGc0giN2yc3JWzff08pPZyFe5TBY-T3Wd6d5-74zpOs0J1jbjfM1Gp5zaKLojebu4ahxEPYJVpZXbxSuMcLys0eOUA2sekkuVTr-l1vVnFp98mkivGF7xcdfBBNVFgEJAcocu8j'];
+var regTokens = ['fdHdJlYZabA:APA91bGv7Y3b6yPHMpsZ5D5cWX6SQjkU9HCovFKfv3J5G1k4JXhHIapC_tPvFTs8Mcrbpnxoavk5AENPs8HNah-k5Lz1H5f0jRC4EnZFaeO4b76ACQ3Ngnn3sop_wkpR1f7IRr7Va-S2'];
 var sender = new gcm.Sender('AIzaSyB9qyp0uN4IqiMIq7SRE8g5B-iOUp8VfS4');
+var projectID="1051405988728";
 sender.send(message, { registrationTokens: regTokens }, function (err, response) {
 	if(err) console.error(err);
 	else 	console.log(response);
