7ea2be5f982970da22aabdf445c79e7fd0461088
Ankit Patel <patel.ankit13@gmail.com>
2016-04-26 21:21:17 -0700

    Added chart.js dependency to display graph on dashboard

--- mean/config/assets/default.js
+++ mean/config/assets/default.js
@@ -5,7 +5,8 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/angular-chart.js/dist/angular-chart.css'
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -16,6 +17,8 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
+        'public/lib/Chart.js/Chart.js',
+        'public/lib/angular-chart.js/dist/angular-chart.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
