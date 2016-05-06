6f997ad50f0011e58a9e13f6a1c9340ba1622c4d
Ishneetkaur <ishneet.kaur@sjsu.edu>
2016-05-05 20:33:54 -0700

    Change in algorithm to include processing last 5 distinct heart rate values and their weight to push notification.

--- LambdaKinesis/version1/src/main/java/lambda_algorithm_twilio/version1/TriggerWebNotification.java
+++ LambdaKinesis/version1/src/main/java/lambda_algorithm_twilio/version1/TriggerWebNotification.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,11 +16,31 @@ import org.json.JSONObject;
 import com.twilio.sdk.TwilioRestException;
 
 public class TriggerWebNotification {
+	private static final int medValue = 72;
 	private static int centerValue = 72;
-	private final static int delta = 9;
+	private static final int delta = 5;
+	private static int lastValue;
+	private static final int MAX_ENTRIES = 5;
 	
+	private static LinkedHashMap<String,Boolean> sensorMap = new LinkedHashMap<String,Boolean>(MAX_ENTRIES + 1, .75F, false) {
+		protected boolean removeEldestEntry(Map.Entry  eldest) {
+            return size() >  MAX_ENTRIES;
+         }
+      };
+	
+	private static LinkedHashMap<Integer,Integer> weightMap = new LinkedHashMap<Integer,Integer>(MAX_ENTRIES + 1, .75F, false) {
+		protected boolean removeEldestEntry(Map.Entry  eldest) {
+            return size() >  MAX_ENTRIES;
+         }
+      };
+    
 	public static void process(String value, String sensorID) throws Exception{
-		if(criticalEstimate(Integer.parseInt(value))){
+		if(!sensorMap.containsKey(sensorID)){
+			System.out.println("Adding Sensor ID: " + sensorID);
+			sensorMap.put(sensorID, true);
+		}
+		
+		if(criticalEstimate(Integer.parseInt(value)) && sensorMap.get(sensorID)){
 		  String url = "http://52.8.186.40/notification";
 		  String param = "{\"fitBitId\":\"" + sensorID + "\", \"value\":\"" + value + "\"}";
 		  String charset = "UTF-8"; 
@@ -45,19 +68,47 @@ public class TriggerWebNotification {
 			  callTwilio(((JSONObject)doctors.get(i)).get("phoneNumber").toString(), value, patientObj.get("displayName").toString(), ((JSONObject)doctors.get(i)).get("displayName").toString());
 		  }
 		  callTwilio(patientObj.get("emergencyContactNumber").toString(), value, patientObj.get("displayName").toString(), patientObj.get("emergencyContactName").toString());
+		  System.out.println("Made Emergency Call for Sensor ID: " + sensorID);
+		  sensorMap.put(sensorID, false);
 		}
 	}
 	
 	private static boolean criticalEstimate(int heartRate) {
-		int difference = heartRate - centerValue;
-		if(Math.abs(difference) >= delta){
-			System.out.println("Critical HeartRate");
-			return true;
+		int difference = heartRate - medValue;
+		
+		if(weightMap.containsKey(heartRate)){
+			int val = weightMap.get(heartRate) + 1;
+			weightMap.remove(heartRate);
+			weightMap.put(heartRate, val);
+			if(((weightMap.get(heartRate) > 1) && (heartRate >= lastValue) && getSeries(heartRate)) || (Math.abs(centerValue - medValue) > 10)){
+				System.out.println("Critical HeartRate: " + heartRate);
+				return true;
+			}
 		}
 		else{
-			centerValue = (centerValue + heartRate)/2 ;
+			if(Math.abs(difference) >= delta){
+				weightMap.put(heartRate, 1);
+			}
 		}
-		System.out.println("No Critical HeartRate" + centerValue);
+		centerValue = (centerValue + heartRate)/2 ;
+		System.out.println("Current Weight Map: " + weightMap);
+		System.out.println("No Critical HeartRate! Current Centroid: " + centerValue + "Current Heart Rate: " + heartRate);
+		lastValue = heartRate;
+		return false;
+	}
+	
+	private static boolean getSeries(int heartRate){
+		int count = 0;
+		Set<Integer> keys = weightMap.keySet();
+		for(int k : keys){
+            if((heartRate - k) > 1){
+            	count++;
+            }
+        }
+
+		if(count > 2){
+			return true;
+		}
 		return false;
 	}
 	
